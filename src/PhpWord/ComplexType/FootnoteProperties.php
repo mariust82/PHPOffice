01e3c0d40aaed96baf7cf1868ef21a28f37fa51a
Marius Ionut Toader <marius.t@gtmarketing.ro>
2023-11-08 18:26:14 +0200

    +Test dev push - analyse - v2

--- src/PhpWord/ComplexType/FootnoteProperties.php
+++ src/PhpWord/ComplexType/FootnoteProperties.php
@@ -181,4 +181,21 @@ final class FootnoteProperties
 
         return $this;
     }
+
+    public static function foo()
+    {
+        #Bug - Reliability - Intentionality rule - high severity
+        if ($this->name != NULL) {
+            // ...
+        }
+    }
+
+    #Clean code attribute - Intentionality; Software Quality - Maintability; Severity - Medium
+    protected function constantsTest()
+    {
+        define('CONSTANT_VALUE', 'old value');
+        define('SCRIPT_DEBUG', 1);
+        // Noncompliant, tries to redefine constant defined 2 lines above
+        define('CONSTANT_VALUE', 'intended value');
+    }
 }
